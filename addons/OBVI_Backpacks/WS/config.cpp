c414564c508f020805223f2e2beafb7f429fb7e2
SiffyFox <Nighthunterlight1@gmail.com>
2026-06-30 17:22:40 -0400

    Fix

--- addons/OBVI_Backpacks/WS/config.cpp
+++ addons/OBVI_Backpacks/WS/config.cpp
@@ -13,7 +13,7 @@ class CfgPatches
         requiredAddons[] = {,
             "Weapons_1_F_lxWS"
         };
-        skipWhenMissingDependencies=1
+        skipWhenMissingDependencies=1;
     };
 };
 
